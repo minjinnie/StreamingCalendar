8efe3631ebde626b7c37bd05e91e45f76bb9898a
Minjin <minjinnie7@gmail.com>
2026-02-24 23:04:45 -0700

    Initial Commit

---
+++ StreamingCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamingCalendar
+//
+//  Created by Minjin Sung on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamingCalendar/StreamingCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingCalendarApp.swift
+//  StreamingCalendar
+//
+//  Created by Minjin Sung on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct StreamingCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
